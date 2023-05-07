2a5b577c054b06df538c4f9fc98693b7dbc7ed69
Diego Astorga <diego.astorga98@gmail.com>
2023-05-07 19:21:04 -0300

    Etapa 2 - Construcción de un Diccionario de Palabras candidatas

---
+++ Etapa2.py
@@ -0,0 +1,93 @@
+import datos #PORQUE ESTA A LA MISMA ALTURA DE RAIZ
+
+
+
+# Ahora el objetivo será generar un diccionario de palabras candidatas a adivinar.
+# Para ello se proveerá una función que retorna un texto en formato:
+# [
+# [“palabra1”, “definición1”],
+# [“palabra2”, “definición2”],
+# …
+# ]
+# Se les proveerá de una función que devolverá un texto del cual extraerán las palabras
+# con sus definiciones para formar el diccionario. Las palabras seleccionadas deberán tener
+# un mínimo de 5 letras.
+# Una vez generado el diccionario de palabras, se debe mostrar por pantalla el total de
+# palabras que hay por cada letra, y el total que hay en el diccionario.
+
+
+def generar_palabras_candidatas ( lista_de_listas ) :  #lista_de_listas, es una lista_de_listasa de lista_de_listasas
+
+    """
+    El objetivo será generar un diccionario de palabras candidatas a adivinar.
+    Las palabras seleccionadas deberán tener un mínimo de 5 letras.
+    Una vez generado el diccionario de palabras, se debe mostrar por pantalla el total de 
+    palabras que hay por cada letra, y el total que hay en el diccionario.
+    """
+    palabras_candidatas = {}
+
+    POSICION_PALABRA = 0
+    MIN_LETRAS = 5
+    CANTIDAD_DE_CANDIDATOS=0
+
+    indice = 0
+
+    while ( CANTIDAD_DE_CANDIDATOS<3 and indice < len (lista_de_listas) ):
+
+        if( len( lista_de_listas[indice][ POSICION_PALABRA] ) >= MIN_LETRAS) :
+
+            palabras_candidatas[lista_de_listas[indice][POSICION_PALABRA]] = lista_de_listas[indice][1]
+
+        indice+=1
+        CANTIDAD_DE_CANDIDATOS+=1
+
+    print( "Esta es la lista por parametros", lista_de_listas[0:3] )
+    print( "Este es mi objeto de candidatos", palabras_candidatas)
+
+    #---------------------------------------------------------------------------------------------
+
+    lista = list( palabras_candidatas.keys() )
+
+    palabras_por_letras= {}
+
+    
+
+    
+    for i in range(len(lista)) :
+
+        contador_De_letras = 0
+        #print( lista[i][0] )
+
+        for j in range(len(lista)) :
+
+            
+            if ( lista[i][0] == lista[j][0] ) :
+                #print('hola')
+                contador_De_letras+=1
+            
+        palabras_por_letras[ lista[i][0] ] = contador_De_letras
+
+    print('Letras y cuantas veces estan: ', palabras_por_letras)
+
+    for elem in palabras_por_letras :
+
+        #print('resulado', elem)
+
+        if ( palabras_por_letras[elem] == 1 ) :
+            print( 'Hay', palabras_por_letras[elem], 'palabra que empieza con la letra', elem )
+        
+        else: 
+
+            print( 'Hay', palabras_por_letras[elem], 'palabras que empiezan con la letra', elem )
+
+    print( lista )
+
+
+
+
+
+    
+
+    print('La cantidad total de palabras candidatas son:', len(palabras_candidatas))
+
+generar_palabras_candidatas( datos.obtener_lista_definiciones() )
