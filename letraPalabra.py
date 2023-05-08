7066fb62beebdc78748a8677c083d33b5b4c41a0
nadeska1 <nademillan0510@gmail.com>
2023-05-07 22:54:56 -0300

    borrado

--- letraPalabra.py
+++
@@ -1,8 +0,0 @@
-from datos import datos_prueba
-
-def letra_palabra():
-    lista_vieja=datos_prueba.datos_prueba()
-    palabra={}
-    for i in range (0,len(lista_vieja)):
-        palabra[lista_vieja[i][0][0].upper()]=lista_vieja[i][0]
-    return(palabra)
--- letraSignificado.py
+++
@@ -1,9 +0,0 @@
-#from datos import datos
-from datos import datos_prueba
-
-def letra_sifnificado():
-    lista_vieja=datos_prueba.datos_prueba()
-    palabra_significado={}
-    for i in range(0,len(lista_vieja)):
-        palabra_significado[lista_vieja[i][0][0].upper()]=lista_vieja[i][1]
-    return (palabra_significado)
