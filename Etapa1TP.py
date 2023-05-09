1136c8fc3e72a6b625a2be96c001de7ad19c5164
agustin <agusmatadordemicheli@gmail.com>
2023-05-09 20:13:58 -0300

    Turnos & historial de partida

--- Etapa1TP.py
+++ Etapa1TP.py
@@ -1,73 +1,76 @@
+import letraPalabra
+import letraSignificado
+
 def tablero(palabras,palabras_significado):
     """
     tablero recibe dos diccionarios, el primero  contiene inciales-palabra,
     el segundo contiene la inicial de la palabra con su significado
     """
-    iniciales=[]
-    palabra_completa=[]
-    respuesta=[]
-    valores=[]
-    letras=[]
-    significado=[]
-    for llaves,valor in palabras.items():
-        iniciales.append(llaves)
-        palabra_completa.append(valor)
-        respuesta.append('[ ]')
-        valores.append(valor)
-    for letters,meaning in palabras_significado.items():
-        letras.append(letters)
-        significado.append(meaning)
-
-    i=0 
-    aciertos=0
-    errores=0
-    PTO_ACIERTO=10
-    PTO_ERROR=-3
+    jugar="si"
     puntos=0
-    turno(iniciales,respuesta,i,aciertos,errores,PTO_ACIERTO,PTO_ERROR,puntos,palabra_completa,significado,valores)
-    juego=input('¿Desea continuar jugando? ')
-    if juego.lower()=='si':
-        turno(iniciales,respuesta,i,aciertos,errores,PTO_ACIERTO,PTO_ERROR,puntos,palabra_completa,significado,valores)
-        puntos=puntos+puntos
-    else:
-        return (f'su puntuacion final es: {puntos}')
+    while jugar=="si":
+        iniciales=[]
+        palabra_completa=[]
+        respuesta=[]
+        valores=[]
+        letras=[]
+        significado=[]
+        for llaves,valor in palabras.items():
+            iniciales.append(f"[{llaves}]")
+            palabra_completa.append(valor)
+            respuesta.append('[ ]')
+            valores.append(valor)
+        for letters,meaning in palabras_significado.items():
+            letras.append(f"[{letters}]")
+            significado.append(meaning)
 
-def turno(iniciales,respuesta,i,aciertos,errores,PTO_ACIERTO,PTO_ERROR,puntos,palabra_completa,significado,valores):
-    while i<len(iniciales):
+        j=0 
+        aciertos=0
+        errores=0
+        PTO_ACIERTO=10
+        PTO_ERROR=-3
+        historial=[]
+        puntos=turno(iniciales,respuesta,j,aciertos,errores,PTO_ACIERTO,PTO_ERROR,puntos,palabra_completa,significado,valores,historial)
+        juego=input('¿Desea continuar jugando? ')
+        if juego.lower()=='no':
+            jugar="no"
+            return (f'su puntuacion final es: {puntos}')
+
+def turno(iniciales,respuesta,j,aciertos,errores,PTO_ACIERTO,PTO_ERROR,puntos,palabra_completa,significado,valores,historial):
+    while j<len(iniciales):
         print("".join(iniciales))
         print("".join(respuesta))
         print("                           ")
         print(f"Aciertos: {aciertos}")
         print(f"Errores: {errores}")
-        print(f"Turno letra {iniciales[i][1]} -palabra de {len(palabra_completa[i])} letras")
-        print(f"Definición: {significado[i]} ")
+        print(f"Turno letra {iniciales[j][1]} -palabra de {len(palabra_completa[j])} letras")
+        print(f"Definición: {significado[j]} ")
         pregunta=input('Ingrese una palabra: ')
         
-        if pregunta ==valores[i]:
-                respuesta[i]='[a]'
-                i+=1
+        
+        if pregunta ==valores[j]:
+                respuesta[j]='[a]'
+                
                 aciertos+=1
                 puntos+=PTO_ACIERTO
-                
+                historial.append(f"Turno letra {iniciales[j][1]}-palabra de {len(palabra_completa[j])} letras - acierto")
+                j+=1
         else:
-                respuesta[i]='[e]'
-                i+=1
+                respuesta[j]='[e]'
                 errores+=1
                 puntos+=PTO_ERROR
+                historial.append(f"Turno letra {iniciales[j][1]}-palabra de {len(palabra_completa[j])} letras - error - palabra correcta: {palabra_completa[j]}")
+                j+=1
+
     print("".join(iniciales))
     print("".join(respuesta))
+    print(listado(historial))
     return puntos
 
-print(tablero({
-        "[A]":'algoritmos',
-        "[B]":'buscar',
-        "[C]":'cauteloso',
-        "[D]":'dedal',
-        "[E]":'estancia',
-    },{
-        "[A]":'s. la mejor materia de la fiuba',
-        "[B]":'v. acción de búsqueda',
-        "[C]":'adj. Indicativo que se refiere a alguien que mantiene cuidado y atención frente a situaciones',
-        "[D]":'s. Elemento utilizado en la costura para protejer el dedo',
-        "[E]":'s. Propiedad que se encuentra alejada de la ciudad, suele estar rodeada de arboles y tiene una granja',
-    }))
+def listado(historial_turno):
+    lista=""
+    for intentos in historial_turno:
+        lista+=intentos+"\n"
+    return lista    
+
+print(tablero(letraPalabra.letra_palabra(),letraSignificado.letra_sifnificado()))
