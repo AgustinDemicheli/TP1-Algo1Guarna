76ac1c8ac9581ded9ef1af7f955065e57725de32
nicolascardone <88558349+nicolascardone@users.noreply.github.com>
2023-05-22 19:43:44 -0300

    comentarios etapa 3

--- Etapa3.py
+++ Etapa3.py
@@ -2,12 +2,22 @@ import random
 import Etapa2
 import datos
 def eleccion_letras():
+    """
+        El objetivo de esta funcion es devolver una lista de 10 letras del abecedario que serán
+    las iniciales de las palabras para el juego
+    - Nicolas Cardone
+    """
     
     abecedario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
                     'x', 'y', 'z']
     return sorted(random.sample(abecedario,10))
     
 def eleccion_palabras_candidatas(diccionario, lista):
+    """"
+        El objetivo de esta funcion es elegir 10 palabras del diccionario formado en la etapa 2
+    las cuales deben comenzar con las letras de la anterior funcion
+    - Nicolas Cardone
+    """
     
     PRIMERA_LETRA = 0
     palabras_candidatas = []
@@ -17,9 +27,6 @@ def eleccion_palabras_candidatas(diccionario, lista):
         if palabra[PRIMERA_LETRA] in lista:
             palabras_candidatas.append(palabra)
             lista.remove(palabra[PRIMERA_LETRA])
-    #print(sorted(palabras_candidatas))
     return sorted(palabras_candidatas)
     
-letras = eleccion_letras()
-
 eleccion_palabras_candidatas( Etapa2.generar_palabras_candidatas(), eleccion_letras() )
