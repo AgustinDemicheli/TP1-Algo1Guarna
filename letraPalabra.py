83e864012a36fb243903bd0bd6b714d91712794b
nicolascardone <88558349+nicolascardone@users.noreply.github.com>
2023-05-22 19:54:41 -0300

    detalles del codigo

--- letraPalabra.py
+++ letraPalabra.py
@@ -2,22 +2,16 @@ import Etapa2
 import Etapa3
 
 def letra_palabra():
-    #lista_vieja=datos_prueba.datos_prueba()
+    PRIMERA_LETRA = 0
     lista_vieja = Etapa3.eleccion_palabras_candidatas( Etapa2.generar_palabras_candidatas(), Etapa3.eleccion_letras() )
     palabra={}
-    for word in lista_vieja:
-        #palabra[lista_vieja[i][0][0].upper()]=lista_vieja[i][0]
-        palabra[word[0]]=word
-
     palabra_significado={}
-
     dicc=Etapa2.generar_palabras_candidatas()
 
     for word in lista_vieja:
-        #palabra[lista_vieja[i][0][0].upper()]=lista_vieja[i][0]
+        palabra[word[PRIMERA_LETRA]]=word
         palabra_significado[word]=dicc[word]
 
-        
     return palabra, palabra_significado
 
 letra_palabra()
